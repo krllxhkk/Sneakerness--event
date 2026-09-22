47c1be75c71b5449bcba09d890fd688ca1c08dc1
aliahaki <341172@student.mboutrecht.nl>
2026-09-22 11:35:34 +0200

    Navigatie homepagina toegevoegd

--- Sneakerness/resources/views/home/index.blade.php
+++ Sneakerness/resources/views/home/index.blade.php
@@ -14,6 +14,31 @@
 </head>
 
 <body>
+<header>
+    <div class="header-container">
+        <div class="logo">SNEAKERNESS<sup>®</sup></div>
+
+        <div class="hamburger" id="hamburgerBtn" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <ul class="nav-links" id="navLinks">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#event">Event</a></li>
+            <li><a href="#tickets">Tickets</a></li>
+            <li><a href="#verkopers">Verkopers</a></li>
+            <li><a href="#stands">Stands</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div class="nav-right">
+            <a href="#" class="login-link">LOGIN</a>
+            <a href="#tickets" class="btn-yellow-sm">TICKET KOPEN</a>
+        </div>
+    </div>
+</header>
 
 </body>
 
