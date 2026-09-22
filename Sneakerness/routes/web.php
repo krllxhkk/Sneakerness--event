591a7e7042db5571162d0adf96dea5b2e0cae462
Ali102105 <alizuhy@outlook.com>
2026-09-22 16:00:08 +0200

    EventController en Model ook gemaakt. en correcte route gezet.

--- Sneakerness/routes/web.php
+++ Sneakerness/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StandController;
+use App\Http\Controllers\EventController;
 
 // Voeg jouw route toe voor de homepagina:
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -10,3 +11,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/stands', [StandController::class, 'index'])
     ->name('stands.index');
+
+Route::get('/events', [EventController::class, 'index'])
+    ->name('events.index');
