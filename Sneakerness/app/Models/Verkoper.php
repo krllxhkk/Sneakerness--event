3ab10e1e82aab3b022930b4258a4b567fc66934a
Karyna <bimchikbananchik@gmail.com>
2026-09-22 13:29:09 +0200

    Create Verkoper model

--- Sneakerness/app/Models/Verkoper.php
+++ Sneakerness/app/Models/Verkoper.php
@@ -3,10 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Verkoper extends Model
 {
+    // Gebruik de juiste tabel
     protected $table = 'Verkoper';
 
+    // Gebruik Id als primary key
+    protected $primaryKey = 'Id';
+
+    // Gebruik geen standaard timestamps
     public $timestamps = false;
+
+    // Haal alle actieve verkopers op
+    public function getVerkopersOverzicht()
+    {
+        return DB::table('Verkoper')
+            ->where('Isactief', 1)
+            ->get();
+    }
 }
