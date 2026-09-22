9b7eacd4814ee03846f981627d6cbdaf3391462e
aliahaki <341172@student.mboutrecht.nl>
2026-09-22 11:34:05 +0200

    Basis structuur homepagina toegevoegd

---
+++ Sneakerness/resources/views/home/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sneakerness® Rotterdam</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;700&family=Inter:wght@400;600;700;900&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
