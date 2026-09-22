8fd8432ac8635c5d19ef198a5832051e345be55d
Karyna <bimchikbananchik@gmail.com>
2026-09-22 17:20:49 +0200

    Add contactpersonen route

--- Sneakerness/routes/web.php
+++ Sneakerness/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\ContactpersoonController;
 // Voeg jouw route toe voor de homepagina:
 Route::get('/', [HomeController::class, 'index'])->name('home');
+// Voeg jouw route toe voor de contactpersonen:
+Route::get('/contactpersonen', [ContactpersoonController::class, 'index'])->name('contactpersonen');
