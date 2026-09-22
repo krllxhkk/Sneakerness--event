7a2ebb032e915b316d4299e35d70d0f460c502d9
Karyna <bimchikbananchik@gmail.com>
2026-09-22 13:35:52 +0200

    Display verkoper data

--- Sneakerness/resources/views/verkopers/index.blade.php
+++ Sneakerness/resources/views/verkopers/index.blade.php
@@ -8,13 +8,27 @@
 
     <h1>Verkopers</h1>
 
+    {{-- Toon alle actieve verkopers --}}
     @foreach($verkopers as $verkoper)
         <div>
+            {{-- Toon de naam van de verkoper --}}
             <h2>{{ $verkoper->Naam }}</h2>
 
-            <p>Soort: {{ $verkoper->VerkooptSoort }}</p>
+            {{-- Toon de gegevens van de verkoper --}}
+            <p>Speciale status: {{ $verkoper->SpecialeStatus ?? 'Geen' }}</p>
+            <p>Verkoopt: {{ $verkoper->VerkooptSoort }}</p>
             <p>Stand type: {{ $verkoper->StandType }}</p>
             <p>Dagen: {{ $verkoper->Dagen }}</p>
+
+            {{-- Toon het logo als het bestaat --}}
+            @if($verkoper->Logo)
+                <p>Logo: {{ $verkoper->Logo }}</p>
+            @endif
+
+            {{-- Toon de opmerking als deze bestaat --}}
+            @if($verkoper->Opmerking)
+                <p>Opmerking: {{ $verkoper->Opmerking }}</p>
+            @endif
         </div>
 
         <hr>
